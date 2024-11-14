f41f75e4725b92d1172c9eda39c3a307117a9571
Nachiketa Prasad <nachiketa.411@gmail.com>
2024-11-14 14:41:37 -0500

    Created a sample web-application for user authentication

---
+++ web-security-sample/web-security-sample/src/main/java/com/example/web_security_sample/WebSecuritySampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_security_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSecuritySampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSecuritySampleApplication.class, args);
+	}
+
+}
---
+++ web-security-sample/web-security-sample/src/test/java/com/example/web_security_sample/WebSecuritySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_security_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSecuritySampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
